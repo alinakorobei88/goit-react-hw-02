c7c84eabea59abbfc4bde04a00cd5b6e0b1072bd
alinakorobei88 <alinakorobei@gmail.com>
2024-07-10 23:44:31 +0200

    chnge2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import Feedback from '../Feedback/Feedback';
-import Options from '../Options/Options';
-import Notification from '../Notification/Notification';
-import styles from './App.module.css'
+import Description from './Description/Description';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification';
+import styles from './App.module.css';
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
 
   useEffect(() => {
-    const storedFeedback = JSON.parse(localStorage.getItem('feedback')) || { good: 0, neutral: 0, bad: 0 };
-    setFeedback(storedFeedback);
+    const savedFeedback = localStorage.getItem('feedback');
+    if (savedFeedback) {
+      setFeedback(JSON.parse(savedFeedback));
+    }
   }, []);
 
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(feedback));
   }, [feedback]);
 
-  const updateFeedback = (feedbackType) => {
-    setFeedback((prevState) => ({
-      ...prevState,
-      [feedbackType]: prevState[feedbackType] + 1,
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
     }));
   };
 
@@ -28,17 +35,28 @@ const App = () => {
   };
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100) || 0;
+  const positiveFeedbackPercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
 
   return (
-    <div className={styles.container}>
-      <h1>Sip Happens Café</h1>
-      <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
-      {totalFeedback ? (
-        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
+    <div className={styles.app}>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positivePercentage={positiveFeedbackPercentage}
+        />
       ) : (
-        <Notification message="No feedback given" />
+        <Notification message="There is no feedback yet" />
       )}
     </div>
   );
---
+++ src/components/App/Description/Description.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Description.module.css';
+
+const Description = () => (
+  <div className={styles.description}>
+    <h1>Sip Happens Café</h1>
+    <p>Please leave your feedback about our service by selecting one of the options below.</p>
+  </div>
+);
+
+export default Description;
